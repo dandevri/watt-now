e390ad16f4495e4421099464a5198f3df11817f6
iancstewart <ian.c.stewart@icloud.com>
2017-05-09 12:20:12 +0200

    :sparkles: Add `server.js`.

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var port = process.env.PORT || '3000';
+var host = process.env.HOST || '0.0.0.0';
+
+var app = express();
+
+app.use(express.static('src'))
+  .set('views', 'views')
+  .set('view engine', 'ejs');
+
+app.listen(port, host, function () {
+  console.log('Server running', host, ':', port);
+});
