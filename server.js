6f5ce9b9eb42f1503b38cd9aca3ebc0404948461
iancstewart <ian.c.stewart@icloud.com>
2017-05-10 14:50:41 +0200

    :wrench: Adjusted server

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.use(express.static('src'))
   .set('views', 'views')
   .set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.get('/dashboard/:user/:festival', function (req, res) {
+  res.render('pages/dashboard');
 });
 
 app.get('/login', function (req, res) {
   res.render('pages/login');
 });
 
-app.get('/festival', function (req, res) {
+app.get('/', function (req, res) {
   res.render('pages/home', {user: 'foodChef', festivals: [{name: 'rollende keukens', date: '10 mei', image: 'http://www.recensiequeens.nl/wp-content/uploads/2014/05/rollende-keukens.jpg'}]});
 });
 
