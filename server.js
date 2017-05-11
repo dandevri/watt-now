7b2bda5918273e9eb2e996fb10fdcc05c2a76369
dandevri <vriesdan@gmail.com>
2017-05-11 19:30:21 +0200

    :sparkles: [R] Add Socket.io server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+var http = require('http');
 var express = require('express');
+var socket = require('socket.io');
 
 var port = process.env.PORT || '3000';
 var host = process.env.HOST || '0.0.0.0';
@@ -35,45 +37,15 @@ app.get('/subscription', function (req, res) {
   res.render('pages/subscription', {label: 'label a', subscription: {cost: '€439,-', size: '2300Kwh'}});
 });
 
-app.listen(port, host, function () {
+var server = http.Server(app);
+var io = socket(server);
+
+server.listen(port, host, function () {
   console.log('Server running', host, ':', port);
 });
 
 function energyLevel(req, res) {
-  var level = req.query.level;
-  console.log('Received energy ' + level + ' request');
-  switch (level) {
-    case '10':
-      res.send('10 procent');
-      break;
-    case '20':
-      res.send('20 procent');
-      break;
-    case '30':
-      res.send('30 procent');
-      break;
-    case '40':
-      res.send('40 procent');
-      break;
-    case '50':
-      res.send('50 procent');
-      break;
-    case '60':
-      res.send('60 procent');
-      break;
-    case '70':
-      res.send('70 procent');
-      break;
-    case '80':
-      res.send('80 procent');
-      break;
-    case '90':
-      res.send('90 procent');
-      break;
-    case '100':
-      res.send('100 procent');
-      break;
-    default:
-      res.send('Procent undefined');
-  }
+  var level = String(Math.round(req.query.level));
+  io.emit('value', level);
+  res.end();
 }
